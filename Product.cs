5ae39fd44baa489b631555ff60ac0f0d9ed2c774
qt3120 <66146935+qt3120@users.noreply.github.com>
2021-02-28 10:48:10 +0700

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeLayerDemo2.Core.Entity
+{
+    class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescriptions { get; set; }
+        public int CategoryID { get; set; }
+        
+        public Product() {
+            this.ProductID = 0;
+            this.ProductName = "";
+            this.ProductDescriptions = "";
+            this.CategoryID = 0;
+        }
+        public String ToString() => this.ProductName;
+    }
+}
